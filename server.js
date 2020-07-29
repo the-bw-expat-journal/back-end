078eb638f7cb820a3cec00b04faf8e318ca1f70f
Kristian Correa <kristiangcorrea@live.com>
2020-07-29 13:54:53 -0400

    Finished Comments endpoints and updated read me.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const authMiddleware = require('./auth/auth-middleware.js')
 const usersRouter = require('./routers/users-router')
 const authRouter = require('./auth/auth-router')
 const postsRouter = require('./routers/posts-router')
+const commentsRouter = require('./routers/comments-router')
 
 server.use(express.json());
 server.use(cors());
@@ -13,6 +14,7 @@ server.use(cors());
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 server.use('/api/posts', authMiddleware, postsRouter)
+server.use('/api/comments', authMiddleware, commentsRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({ status: "Server is up and running!"})
